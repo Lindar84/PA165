5d46185e7d44b37a80f5af658c353ed23005c34a
Ludmila Fialová <xfialov3@fi.muni.cz>
2017-12-07 06:24:03 +0100

    rest_step5

--- eshop-rest/src/main/java/cz/fi/muni/pa165/RootWebContext.java
+++ eshop-rest/src/main/java/cz/fi/muni/pa165/RootWebContext.java
@@ -2,12 +2,15 @@ package cz.fi.muni.pa165;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import cz.fi.muni.pa165.service.config.ServiceConfiguration;
 import cz.muni.fi.pa165.sampledata.EshopWithSampleDataConfiguration;
+
+import java.text.SimpleDateFormat;
 import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
@@ -16,6 +19,10 @@ import org.springframework.http.converter.json.MappingJackson2HttpMessageConvert
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * https://spring.io/blog/2014/12/02/latest-jackson-integration-improvements-in-spring
+ */
 
 @EnableWebMvc
 @Configuration
@@ -39,7 +46,11 @@ public class RootWebContext extends WebMvcConfigurerAdapter {
         MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-       
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+       // use setDateFormat either using SimpleDateFormat or com.fasterxml.jackson.databind.util.ISO8601DateFormat
+        // so dates are now in a more human-readable format
+        objectMapper.setDateFormat(new SimpleDateFormat("dd/MM/yyyy"));
+
         jsonConverter.setObjectMapper(objectMapper);
         return jsonConverter;
     }
